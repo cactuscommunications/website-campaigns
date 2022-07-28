52fe42d3957c2edc5862787d5240d114716f39ff
ratul-cactus <ratul.mukhopadhyay@cactusglobal.com>
2022-07-28 21:21:47 +0530

    DRG-73820 Service Information Ruby component

---
+++ apps/japan/src/app/components/service-information-ruby/models.ts
@@ -0,0 +1,61 @@
+export interface IServiceInformationRuby {
+    heading?: string;
+    subHeading?: string;
+    card: ICardInfo[]
+    path: string
+  }
+
+  export interface IServiceInfo {
+      path:  string,
+      text: string
+  }
+
+  export interface ICardInfo {
+    path:  string,
+    heading: IHeading;
+    desc: string;
+    benefit: IBenefit;
+    listHeading: string;
+    list:IServiceInfoList[]
+    pricing: IPricing;
+    }  
+    
+    export interface IServiceInfoList {
+        
+        path: string;
+        text: string;
+    }
+
+    export interface IHeading {
+        heading: string;
+        specialHeadingText: string;
+        headingClassName: string;
+        subHeadingClass: string;
+        comment: string;
+        path: string
+
+    }
+    export interface IBenefit {
+        text: string
+        className: string
+        path:string
+    }
+    export interface IPricing {
+        path1: string;
+        path2: string;
+        wordCount: string;
+        days: string;
+        text: string;
+        disclaimer: string;
+        price: string;
+        word: string;
+        tax: string;
+        CTAdetails: string;
+
+    }
+
+export interface IServiceInfoList {
+    path: string;
+    text: string;
+}
+ 
