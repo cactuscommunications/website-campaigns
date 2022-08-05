94d216925676dd3104f86e6d47dafa359243ccb1
Goutham <goutham.reddy@codevyasa.com>
2022-08-05 19:57:05 +0530

    Added dynamic header and meta data

--- apps/japan/src/app/pages/thank-you.tsx
+++ apps/japan/src/app/pages/thank-you.tsx
@@ -1,10 +1,20 @@
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
 import ThankYouRuby from '../components/thank-you-ruby/thank-you-ruby';
+// @ts-ignore
+import { Helmet } from "react-helmet";
+const params = {
+  title: ' 分野別の英文校正サービス｜エディテージ',
+  description: '英文校正は専門分野が命です。1300分野で分野別のネイティブ2名以上がお客様の学術論文を校正し、受理率を最大限に高めます。英文校正業界で第1位のエディテージが提供する学術英文校正サービス。'
+}
 
 export function ThankYou() {
   return (
     <>
+      <Helmet>
+        <title>{params.title}</title>
+        <meta name="description" content={params.description} />
+      </Helmet>
       <Header />
       <ThankYouRuby />
       <Footer />
