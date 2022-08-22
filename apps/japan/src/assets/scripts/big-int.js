363dbe4986555ce5a069230773c1aeeb03e51b5a
abhijit.kadam <abhijit.kadam@cactusglobal.com>
2022-08-23 03:45:52 +0530

    Bigint script integration
    (cherry picked from commit cf04508aa7d78b7957417c370b64a19fda829866)

---
+++ apps/japan/src/assets/scripts/big-int.js
@@ -0,0 +1,20 @@
+//BigInt tracking
+(function (bi, gi, nt) {
+    window._bigint = {
+        "track": function (l, de, da) {
+            window._bigint._oB.push({"l": l, "de": de, "da": da});
+        }, "_oB": []
+    };
+    var _sD = ((/(test|local|dev|staging)[0-9]*\./gi).test(window.location.hostname)) ? "test1." : "";
+    _sD = (nt === undefined || nt === "") ? _sD : (nt + ".");
+    var _sE = (bi === undefined || bi === "") ? "cactusglobal.io" : bi;
+    var _aE = (gi === undefined || gi === "") ? "cactusglobal.io" : gi;
+    window.cactusglobal_io = {};
+    window.cactusglobal_io.endpoint = "https://" + _sD + _aE + "/v1/";
+    var d = new Date();
+    var jsElm = document.createElement("script");
+    jsElm.async = 1;
+    jsElm.type = "application/javascript";
+    jsElm.src = "https://" + _sD + _sE + "/assets/client-track.js?" + d.getFullYear() + '' + d.getMonth() + '' + d.getDate() + '' + d.getHours();
+    document.head.appendChild(jsElm);
+})("cf.cactusglobal.io", "api.cactusglobal.io");
