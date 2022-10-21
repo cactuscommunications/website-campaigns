aa5ca8a00bc8df1f159a14e094c2def15075b4c1
chenisha <chenisha.shah@cactusglobal.com>
2022-10-21 14:00:22 +0530

    DN-23579 - [Japan PPC] Introduce PES 2-year MRE add-on

--- apps/japan/src/app/components/service-information-ruby/service-info-list.tsx
+++ apps/japan/src/app/components/service-information-ruby/service-info-list.tsx
@@ -1,18 +1,31 @@
 import MarkDown from '../markdown/markdown';
 interface IServiceInfo {
     path:  string,
-    text: string
+    text: string,
+    color?: boolean
 }
 
 export function ServiceInfoList({ list }: { list: IServiceInfo }) {
   return (
     <li className="mb-4 sm:mb-2.5 flex items-center">
-      <span className="h-4 w-4 inline-block bg-no-repeat bg-contain mr-3 sm:mr-2 mt-1 sm:mt-0 flex-shrink-0"
-        style={{ backgroundImage: `url(${list.path})`, }}></span>
-      <span className="inline-block text-base sm:text-sm sm:leading-4 text-ruby-alpha font-sb">
-        <MarkDown data={list.text}></MarkDown>
-      </span>
-    </li>         
+      <span
+        className="h-4 w-4 inline-block bg-no-repeat bg-contain mr-3 sm:mr-2 mt-1 sm:mt-0 flex-shrink-0"
+        style={{ backgroundImage: `url(${list.path})` }}
+      ></span>
+      {list.color ? (
+        <span className="inline-block text-base sm:text-sm sm:leading-4 text-ruby-alpha font-sb">
+          <MarkDown data={list.text.split("!!break!!")[0]}></MarkDown>
+          <br />
+          <span className="text-red-700">
+            {list.text.split("!!break!!")[1]}
+          </span>
+        </span>
+      ) : (
+        <span className="inline-block text-base sm:text-sm sm:leading-4 text-ruby-alpha font-sb">
+          <MarkDown data={list.text}></MarkDown>
+        </span>
+      )}
+    </li>
   );
 }
 
