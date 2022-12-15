2485942d5867992f9ee119542750e341d4de822c
hardi <hardi.shah@mobiosolutions.com>
2022-12-15 15:29:55 +0530

    DRG-80812 - CR feedback

--- apps/japan/src/app/components/listing-ruby/listing-ruby.tsx
+++ apps/japan/src/app/components/listing-ruby/listing-ruby.tsx
@@ -83,7 +83,68 @@ const ListingRuby = ({ params , searchText}: { params: IListingRubyParams, searc
   }
   return (
     <>
-
+      <section className='bg-primary pb-10 pt-12 sm:pt-9 sm:pb-7'>
+        <div className='container'>
+            <div className='font-sb mb-7 text-4xl text-center leading-50 md:text-3xl sm:text-2xl sm:leading-9 sm:text-left sm:mb-3'>ニーズに合わせた3種類の英文校正支援サービス</div>
+            <p className='font-sb text-base'>
+                エディテージの校正レベルは３段階 。必要に応じて一番最適なものをお選びください。
+                <span className='block sm:inline'>付属する無料サービスや、追加できるオプション なども変わります。</span>
+            </p>
+            <div className='flex items-end justify-between sm:flex-col'>
+                <div className='w-4/12 sm:w-full'>
+                    <p className='text-xl font-sb mb-5 md:text-base sm:text-base sm:text-white sm:bg-pearl-alpha sm:py-2.5 sm:px-3 sm:mb-2 sm:mt-7'>スタンダード英文校正</p>
+                    <p className='relative text-white bg-pearl-alpha w-10/12 flex items-center justify-center px-5 py-2.5 mb-3 text-base leading-6 font-sb md:text-sm sm:text-black sm:bg-inherit sm:w-full sm:h-auto sm:p-0 sm:justify-start'>
+                        語彙・文法をネイティブ校正者2名がチェック
+                        <span className='absolute w-5 h-full border-[36px] border-l-[20px] border-solid border-transparent border-l-pearl-alpha right-[-56px] sm:hidden'></span>
+                    </p>
+                    <a className='relative text-white bg-garnet-mu text-center rounded w-96 p-2.5 mt-7 text-lg font-sb md:text-sm sm:mt-6 sm:w-full sm:mb-2.5'>
+                        スタンダード英文校正の詳細
+                      <span className='absolute w-5 h-5 right-5 inset-y-0 m-auto inline-block bg-no-repeat md:w-4 md:h-4 md:bg-cover sm:w-3 sm:h-3 sm:bg-cover sm:right-3' style={{backgroundImage: `url(${'/assets/images/icons/button-icon.svg'})`}}></span>
+                    </a>
+                </div>
+                <div className='w-4/12 sm:w-full'>
+                    <p className='text-xl font-sb mb-5 md:text-base sm:text-base sm:text-white sm:bg-pearl-alpha sm:py-2.5 sm:px-3 sm:mb-2 sm:mt-7'>スタンダード英文校正</p>
+                    <p className='relative text-white bg-pearl-alpha w-10/12 flex items-center justify-center px-5 py-2.5 mb-3 text-base font-sb md:text-sm sm:font-normal sm:text-black sm:bg-inherit sm:w-full sm:h-auto sm:p-0 sm:justify-start'>
+                        段落レベルの論理構造まで見直し、 論旨をより明確に伝える
+                        <span className='absolute w-5 h-full border-[36px] border-l-[20px] border-solid border-transparent border-l-pearl-alpha right-[-56px] sm:hidden'></span>
+                    </p>
+                    <p className='relative text-black bg-white w-10/12 h-16 flex items-center justify-center px-5 py-2.5 mb-3 border border-pearl-alpha text-base md:text-sm sm:text-black sm:bg-inherit sm:w-full sm:h-auto sm:p-0 sm:justify-start sm:border-0 sm:font-sb sm:mt-8 sm:font-sb'>
+                        語彙・文法をネイティブ校正者2名がチェック
+                        <span className='hidden sm:inline-block absolute text-2xl inset-x-0 top-0 -mt-10 m-auto w-3 h-3 '>+</span>
+                        <span className='absolute w-5 h-full border-[33px] border-l-[20px] border-solid border-transparent border-l-white right-[-53px] z-10 sm:hidden'></span>
+                        <span className='absolute w-5 h-full border-[33px] border-l-[20px] border-solid border-transparent border-l-pearl-alpha right-[-54px] sm:hidden'></span>
+                    </p>
+                    <a className='relative text-white bg-garnet-mu text-center rounded w-96 p-2.5 mt-7 text-lg font-sb md:text-sm sm:mt-6 sm:w-full sm:mb-2.5'>
+                        プレミアム英文校正の詳細
+                        <span className='absolute w-5 h-5 right-5 inset-y-0 m-auto inline-block bg-no-repeat md:w-4 md:h-4 md:bg-cover sm:w-3 sm:h-3 sm:bg-cover sm:right-3' style={{backgroundImage: `url(${'/assets/images/icons/button-icon.svg'})`}}></span>
+                    </a>
+                </div>
+                <div className='w-4/12 sm:w-full'>
+                    <p className='text-xl font-sb mb-5 md:text-base sm:text-base sm:text-white sm:bg-pearl-alpha sm:py-2.5 sm:px-3 sm:mb-2 sm:mt-7'>スタンダード英文校正</p>
+                    <p className='relative text-white bg-pearl-alpha w-10/12 flex items-center justify-center px-5 py-2.5 mb-3 text-base font-sb md:text-sm sm:text-black sm:font-normal sm:bg-inherit sm:w-full sm:h-auto sm:p-0 sm:justify-start'>
+                        査読経験校正者が、研究内容まで踏み込んで論文校正
+                        <span className='absolute w-5 h-full border-[36px] border-l-[20px] border-solid border-transparent border-l-pearl-alpha right-[-56px] sm:hidden'></span>
+                    </p>
+                    <p className='relative text-black bg-white w-10/12 h-16 flex items-center justify-center px-5 py-2.5 mb-3 border border-pearl-alpha text-base md:text-sm sm:font-normal sm:text-black sm:bg-inherit sm:w-full sm:h-auto sm:p-0 sm:justify-start sm:border-0 sm:font-sb sm:mt-8'>
+                        段落レベルの論理構造まで見直し、論旨をより明確に伝える
+                        <span className='hidden sm:inline-block absolute text-2xl inset-x-0 top-0 -mt-10 m-auto w-3 h-3 '>+</span>
+                        <span className='absolute w-5 h-full border-[33px] border-l-[20px] border-solid border-transparent border-l-white right-[-53px] z-10 sm:hidden'></span>
+                        <span className='absolute w-5 h-full border-[33px] border-l-[20px] border-solid border-transparent border-l-pearl-alpha right-[-54px] sm:hidden'></span>
+                    </p>
+                    <p className='relative text-black bg-white w-10/12 h-16 flex items-center justify-center px-5 py-2.5 mb-3 border border-pearl-alpha text-base md:text-sm sm:text-black sm:bg-inherit sm:w-full sm:h-auto sm:p-0 sm:justify-start sm:border-0 sm:font-sb sm:mt-8 sm:font-sb'>
+                        語彙・文法をネイティブ校正者2名がチェック
+                        <span className='hidden sm:inline-block absolute text-2xl inset-x-0 top-0 -mt-10 m-auto w-3 h-3 '>+</span>
+                        <span className='absolute w-5 h-full border-[33px] border-l-[20px] border-solid border-transparent border-l-white right-[-53px] z-10 sm:hidden'></span>
+                        <span className='absolute w-5 h-full border-[33px] border-l-[20px] border-solid border-transparent border-l-pearl-alpha right-[-54px] sm:hidden'></span>
+                    </p>
+                    <a className='relative text-white bg-garnet-mu text-center rounded w-96 p-2.5 mt-7 text-lg font-sb md:text-sm sm:mt-6 sm:w-full sm:mb-2.5'>
+                        トップジャーナル英文校正の詳細
+                        <span className='absolute w-5 h-5 right-5 inset-y-0 m-auto inline-block bg-no-repeat md:w-4 md:h-4 md:bg-cover sm:w-3 sm:h-3 sm:bg-cover sm:right-3' style={{backgroundImage: `url(${'/assets/images/icons/button-icon.svg'})`}}></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+      </section>
       <section className={(params.hideHeading ? 'bg-pearl-zeta' : 'bg-primary') + ' pt-8 pb-10 '}  >
         <div className="container">
 
